ef0e592f7aa7391f77dd6d980688cadfa4566b9e
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-07-21 14:35:16 +0530

    Update cipher.py

--- AA1/cipher.py
+++ AA1/cipher.py
@@ -1,56 +1,52 @@
 capitalLetters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lowerLetters='abcdefghijklmnopqrstuvwxyz'
 numbers='0123456789'
-# Create string of special characters
+# Create a list AllCharacters
 
 
 def cipher(plaintext, n):
-    # Access specials as global
+    # Access AllCharacters as global
     global  capitalLetters, lowerLetters, numbers
     ciphertext = ''
 
     for char in plaintext:
+        # Check if char exits in AllCharacters
         if char in numbers:
+            # Find position of char in AllCharacters
             currentPosition = numbers.find(char)
+            # Calculate the cipher text as AllCharacters[(currentPosition + n ) % 77]
             ciphertext += numbers[(currentPosition + n ) % 10]
+        # Remove rest of the elif's
         elif char in lowerLetters:
             currentPosition = lowerLetters.find(char)
             ciphertext += lowerLetters[(currentPosition + n )% 26] 
         elif char in capitalLetters:
             currentPosition = capitalLetters.find(char)
             ciphertext += capitalLetters[(currentPosition + n) % 26]
-        # Check if char exits in specials  
-        
-            # Get position of the char in specials   
-            
-            # Get the new cha using formula : specials[(currentPosition + n) % 15], and add it to ciphertext
-            
         else:
             ciphertext += char
 
     return ciphertext
         
 def decipher(ciphertext, n):
-    # Access specials as global
+    # Access AllCharacters as global
     global capitalLetters, lowerLetters, numbers
     plaintext = ""
 
     for char in ciphertext:
+         # Check if char exits in AllCharacters
         if char in numbers:
+            # Find position of char in AllCharacters
             currentPosition = numbers.find(char)
+            # Calculate the cipher text as AllCharacters[(currentPosition - n ) % 77]
             plaintext += numbers[(currentPosition - n) % 10]
+        # Remove rest of the elif's
         elif char in lowerLetters:
             currentPosition = lowerLetters.find(char)
             plaintext += lowerLetters[(currentPosition - n )% 26] 
         elif char in capitalLetters:
             currentPosition = capitalLetters.find(char)
             plaintext += capitalLetters[(currentPosition - n) % 26] 
-        # Check if char exits in specials  
-        
-            # Get position of the char in specials 
-            
-            # Get the new cha using formula : specials[(currentPosition - n) % 15], and add it to plaintext
-                   
         else:
             plaintext += char
             
